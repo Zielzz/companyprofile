20cbaf8da1768f7380ffc96da4794b63ea103238
Zielzz <naufalzyram@gmail.com>
2023-01-30 10:28:03 +0700

    mengubah isi index

--- about.php
+++ about.php
@@ -63,10 +63,10 @@
 <body>
 
   <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top ">
+  <header id="header" class="bg-dark ">
     <div class="container d-flex align-items-center">
 
-      <div class="logo me-auto"><a href="index.html">COMPANY</a></div>
+      <div class="logo me-auto"><a href="index.html">SCHOOL</a></div>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
@@ -83,23 +83,7 @@
     </div>
   </header><!-- End Header -->
 
-  <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center">
-
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-          <h1>Better Solutions For Your Business</h1>
-          <h2>We are team of talented designers making websites with Bootstrap</h2>
-          <div class="d-flex justify-content-center justify-content-lg-start">
-            <a href="#about" class="btn-get-started scrollto">Get Started</a>
-          </div>
-        </div>
-      </div>
-    </div>
-
-  </section>
-  <!-- End Hero -->
+ 
 
   <br><br><br><br><br><br>
   
--- proses/list.php
+++ proses/list.php
@@ -82,10 +82,10 @@ $results = mysqli_fetch_all($query, MYSQLI_ASSOC);
 <body>
 
   <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top ">
+  <header id="header" class="bg-dark ">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a style="text-decoration:none" href="../index.html">COMPANY</a></h1>
+      <h1 class="logo me-auto"><a style="text-decoration:none" href="../index.html">SCHOOL</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
@@ -102,26 +102,10 @@ $results = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
   </header><!-- End Header -->
 
-  <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center">
-
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-          <h1>Better Solutions For Your Business</h1>
-          <h2>We are team of talented designers making websites with Bootstrap</h2>
-          <div class="d-flex justify-content-center justify-content-lg-start">
-            <a style="text-decoration:none" href="#about" class="btn-get-started scrollto">Get Started</a>
-          </div>
-        </div>
-      </div>
-    </div>
-
-  </section>
-  <!-- End Hero -->
+  
 
 
-  <br><br><br><br><br><br>
+  <br><br>
   
     <!-- ======= About Us Section ======= -->
     <div id="about" class="about">
